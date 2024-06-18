330c219560ad6e0fe9969fb2059d7640b208ae3e
jirijanecek <jiri.janecek@yahoo.com>
2024-06-18 15:18:10 +0200

    Fixed party row cell layout

--- VoteVault/Views/PartyRowView.swift
+++ VoteVault/Views/PartyRowView.swift
@@ -11,35 +11,42 @@ struct PartyRowView: View {
     private let model: PartyModel
     
     var body: some View {
-        HStack {
-            Text(self.model.name ?? "")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            
-            Spacer(minLength: 10)
-            
-            RoundedRectangle(cornerRadius: 5)
-                .frame(width: 30, height: 30)
-                .foregroundStyle(self.model.color)
-            
-            Spacer()
-                .frame(width: 10)
-            
-            Text(verbatim: "\(self.model.formattedPercentsValue)%")
-                .font(.title2)
-                .fontWeight(.semibold)
-                .multilineTextAlignment(.trailing)
+        GeometryReader { geometry in
+            HStack {
+                Text(self.model.name ?? "")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Spacer(minLength: 10)
+                
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(width: 30, height: 30)
+                    .foregroundStyle(self.model.color)
+                
+                HStack {
+                    Spacer()
+                    
+                    Text(verbatim: "\(self.model.formattedPercentsValue)%")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                }
+                .frame(width: geometry.size.width * 0.25)
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 5)
+            // note: takto udělané pozadí? smart! :)
+            .background(
+                RoundedRectangle(cornerRadius: 5)
+                    .foregroundStyle(Color(.systemGray6))
+            )
         }
-        .padding(.horizontal, 10)
-        .padding(.vertical, 5)
-        // note: takto udělané pozadí? smart! :)
-        .background(
-            RoundedRectangle(cornerRadius: 5)
-                .foregroundStyle(Color(.systemGray6))
-        )
     }
     
     init(model: PartyModel) {
         self.model = model
     }
+}
+
+#Preview {
+    PartyRowView(model: PartyModel(name: "ODS", percents: 15.7, color: .black, isCoaliton: false))
 }
