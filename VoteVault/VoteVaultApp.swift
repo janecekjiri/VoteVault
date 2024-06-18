219eaa386efe5d4d8fabea6a1f2b7841698d1067
jirijanecek <jiri.janecek@yahoo.com>
2024-06-18 09:14:14 +0200

    Initial Commit

---
+++ VoteVault/VoteVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoteVaultApp.swift
+//  VoteVault
+//
+//  Created by Jiri Janecek on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct VoteVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
